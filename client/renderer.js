8ae947badc5d1506cc1e9cd12278f97c9d9c9c02
Carlos Alba <calba114@Carloss-MacBook-Pro.local>
2019-06-11 21:37:04 -0700

    other information

--- client/renderer.js
+++ client/renderer.js
@@ -1,9 +1,9 @@
 const { exec } = require("child_process");
+const { dialog } = require("electron");
 
-const temp = setInterval(() => {
-  const query = `"SELECT key, name, celsius, fahrenheit FROM temperature_sensors;"`;
+function system_info() {
+  const query = `"SELECT * from system_info;"`;
   const prefix = "osqueryi --json";
-  clearInterval(temp);
   const mainCommand = `${prefix} ${query}`;
   exec(mainCommand, (err, stdout, stderr) => {
     if (err) {
@@ -12,25 +12,134 @@ const temp = setInterval(() => {
     }
     const links = document.querySelector("#activeForm");
     let data = JSON.parse(stdout);
+    links.innerHTML = "";
     data.forEach(x => {
       links.innerHTML += `
-        <li class="list-group-item">
-        <div class="media-body">
-          <strong>
-            ${x.name}
-          </strong>
-          <p>
-          fahrenheit: ${x.fahrenheit}
-          </p>
-          <p>
-          celsius: ${x.celsius}
-          </p>
-          <p>
-          key: ${x.key}
-          </p>
-        </div>
-      </li>
-        `;
+                    ${JSON.stringify(x)}
+              `;
     });
   });
-}, 10);
+}
+
+function temperature_sensors() {
+  const query = `"SELECT key, name, celsius, fahrenheit FROM temperature_sensors;"`;
+  const prefix = "osqueryi --json";
+  const mainCommand = `${prefix} ${query}`;
+  exec(mainCommand, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const links = document.querySelector("#activeForm");
+    let data = JSON.parse(stdout);
+    links.innerHTML = "";
+    data.forEach(x => {
+      links.innerHTML += `
+          <li class="list-group-item">
+          <div class="media-body">
+            <strong>
+              ${x.name}
+            </strong>
+            <p>
+            fahrenheit: ${x.fahrenheit}
+            </p>
+            <p>
+            celsius: ${x.celsius}
+            </p>
+            <p>
+            key: ${x.key}
+            </p>
+          </div>
+        </li>
+          `;
+    });
+  });
+}
+
+function uptime() {
+  const query = `" SELECT days, hours, minutes, seconds, total_seconds FROM uptime;"`;
+  const prefix = "osqueryi --json";
+  const mainCommand = `${prefix} ${query}`;
+  exec(mainCommand, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const links = document.querySelector("#activeForm");
+    let data = JSON.parse(stdout);
+    links.innerHTML = "";
+    data.forEach(x => {
+      links.innerHTML += `
+                    ${JSON.stringify(x)}
+              `;
+    });
+  });
+}
+
+function wifi() {
+  const query = `" SELECT network_name, last_connected, security_type, captive_portal FROM wifi_networks;"`;
+  const query2 = `"SELECT network_name, security_type, mode, channel, rssi, interface FROM wifi_status;"`;
+  const prefix = "osqueryi --json";
+  const objects = [];
+  const mainCommand = `${prefix} ${query}`;
+  const mainCommand2 = `${prefix} ${query2}`;
+
+  exec(mainCommand, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    objects.push(stdout);
+  });
+
+  exec(mainCommand2, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    objects.push(stdout);
+  });
+
+  const links = document.querySelector("#activeForm");
+  links.innerHTML = "";
+  objects.forEach(x => {
+    links.innerHTML = `
+        ${JSON.stringify(x)}
+        `;
+  });
+}
+
+// const temp = setInterval(() => {
+//   const query = `"SELECT key, name, celsius, fahrenheit FROM temperature_sensors;"`;
+//   const prefix = "osqueryi --json";
+//   clearInterval(temp);
+//   const mainCommand = `${prefix} ${query}`;
+//   exec(mainCommand, (err, stdout, stderr) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     const links = document.querySelector("#activeForm");
+//     let data = JSON.parse(stdout);
+//     data.forEach(x => {
+//       links.innerHTML += `
+//         <li class="list-group-item">
+//         <div class="media-body">
+//           <strong>
+//             ${x.name}
+//           </strong>
+//           <p>
+//           fahrenheit: ${x.fahrenheit}
+//           </p>
+//           <p>
+//           celsius: ${x.celsius}
+//           </p>
+//           <p>
+//           key: ${x.key}
+//           </p>
+//         </div>
+//       </li>
+//         `;
+//     });
+//   });
+// }, 10);
