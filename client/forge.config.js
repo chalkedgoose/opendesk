51c94f55fff06e43ba5f65aed08a337ded81b53b
Carlos Alba <calba114@Carloss-MacBook-Pro.local>
2019-06-12 20:42:09 -0700

    electron forge config skeleton

---
+++ client/forge.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  makers: [
+    {
+        name: '@electron-forge/maker-zip',
+        platforms: ['darwin', 'linux'],
+        config: {
+          
+        }
+    }
+  ]
+}
