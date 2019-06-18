7601e2b3ce0db11404fb5f90293616fb0ab93484
Carlos Alba <soradev4@gmail.com>
2019-06-17 23:41:07 -0700

    psquery

--- dependencies.sh
+++ dependencies.sh
@@ -12,12 +12,11 @@ cd opendesksetup
 
 # function setup
 download_dependencies(){
+    # pull osquery from facebooks pkg repo
+    curl -o dependency.pkg  https://pkg.osquery.io/darwin/osquery-3.3.2.pkg
 
 }
 
-
-# pull pkg from facebooks repo
-curl -o dependency.pkg  https://pkg.osquery.io/darwin/osquery-3.3.2.pkg
 echo "dependency installed! wait for the .zip with opendesk in it"
 curl -o opendesk.zip https://github.com/calba5141114/opendesk/blob/master/dist/opendesk.zip
 echo "Go to your desktop open openDeskDownloads"
