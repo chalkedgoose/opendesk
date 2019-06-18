8338b3b8b29eed54ff90c9b5885b9cd69de987cc
Carlos Alba <soradev4@gmail.com>
2019-06-17 23:45:04 -0700

    osquery if

--- dependencies.sh
+++ dependencies.sh
@@ -14,12 +14,17 @@ cd opendesksetup
 download_dependencies(){
     # pull osquery from facebooks pkg repo
     curl -o dependency.pkg  https://pkg.osquery.io/darwin/osquery-3.3.2.pkg
+    if [ -f dependency.pkg ]
+    then
+        echo "osquery downloaded"
+    else
+        echo "osquery download failed"
+    fi
     # pull opendesk app from github repo
     curl -o opendeskapp.zip https://github.com/calba5141114/opendesk/blob/master/dist/opendesk.zip
 }
 
-echo "dependency installed! wait for the .zip with opendesk in it"
-curl -o opendesk.zip https://github.com/calba5141114/opendesk/blob/master/dist/opendesk.zip
+
 echo "Go to your desktop open openDeskDownloads"
 echo "run dependency.pkg and then \n"
 echo "unzip .zip and drop open desk into applications folder \n"
