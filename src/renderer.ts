8e3cf5598fade3380692a10f91ee99fa50fd8d25
Carlos Alba <soradev4@gmail.com>
2019-09-17 21:34:33 -0700

    "change"

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,8 +1,11 @@
 import { exec } from "child_process";
 
 const elements = ["wifi", "system_info", "temperature", "uptime", "feedback"];
-async function rmAttributes(){
+async function rmAttributes(): Promise<void> {
     elements.forEach(id => {
-
+        document
+        .querySelector(`${id}`)
+        .classList
+        .remove("active");
     });
 }
