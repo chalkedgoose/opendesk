666ddf1e871ad0e83beeb420ec3e5594b263f662
Carlos Alba <soradev4@gmail.com>
2019-09-17 21:56:38 -0700

    change

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,5 +1,9 @@
 
 export interface ISystemInterface {
     computer_name?: string;
-
+    cpu_brand?: string;
+    cpu_logical_cores?: string;
+    cpu_physical_cores?: string;
+    cpu_subtype?: string;
+    cpu_type?: string;
 }
